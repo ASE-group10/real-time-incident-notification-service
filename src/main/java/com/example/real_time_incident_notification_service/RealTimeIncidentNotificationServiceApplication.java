b1bd597878bcaf74a66366d0f9c452a14b056bd7
Zhaocun <zhaocunsun@gmail.com>
2024-11-15 11:28:27 +0000

    Initial Maven Spring Boot setup for real-time-incident-notification-service

---
+++ src/main/java/com/example/real_time_incident_notification_service/RealTimeIncidentNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_time_incident_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeIncidentNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeIncidentNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real_time_incident_notification_service/RealTimeIncidentNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_time_incident_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeIncidentNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
